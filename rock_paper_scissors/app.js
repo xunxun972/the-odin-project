5fcf40b48f7f00eb739aa133e844c43595be07a4
xunxun972 <xunxun972@protonmail.com>
2021-09-01 11:29:15 +0700

    i

---
+++ rock_paper_scissors/app.js
@@ -0,0 +1,17 @@
+import './app.css';
+
+const computerPlay = () => {
+    let r = Math.floor(Math.random() * 3);
+    console.log(r);
+    return "Rock";
+}
+
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
+computerPlay();
